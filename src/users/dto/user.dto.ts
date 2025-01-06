9d77b092f5796c3c7d666390b0bc1d6b0cea0fe2
ChoboDev <gealot@gmail.com>
2025-01-06 16:55:01 +0900

    ➕ Chore(deps): add cuid2 deps
    cuid2 라이브러리를 추가하였습니다.

---
+++ src/users/dto/user.dto.ts
@@ -0,0 +1,75 @@
+import { ApiProperty, OmitType } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+// 사용자 정보 DTO
+export class UserDto {
+  @ApiProperty({
+    nullable: false,
+    description: '사용자 ID (CUID 자동 생성)',
+    example: '',
+    type: String,
+  })
+  id: number;
+
+  @ApiProperty({
+    nullable: false,
+    description: '이메일(최소 5자, 최대 254자, 이메일 형식)',
+    example: 'test@test.dev',
+    type: String,
+  })
+  @IsEmail({}, { message: '올바른 이메일 형식이어야 합니다.' })
+  @IsNotEmpty({ message: '이메일은 필수 입력 항목입니다.' })
+  @MinLength(5, { message: '이메일은 최소 5자 이상 입력해야 합니다.' })
+  @MaxLength(254, { message: '이메일은 최대 254자까지 입력할 수 있습니다.' })
+  // 이메일 제약조건: 최소 5자, 최대 254자, 이메일 형식
+  email: string;
+
+  @ApiProperty({
+    nullable: false,
+    description:
+      '비밀번호(해싱 전, 최소 8자, 최대 128자, 영문, 숫자, 특수문자 포함 문자열)',
+    example: 'P@ssw0rd!',
+    type: String,
+  })
+  @IsString({ message: '비밀번호는 문자열이어야 합니다.' })
+  @MinLength(8, { message: '비밀번호는 최소 8자 이상이어야 합니다.' })
+  @MaxLength(128, { message: '비밀번호는 최대 128자 이하여야 합니다.' })
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])/, {
+    message: '비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.',
+  })
+  // 비밀번호 제약조건: 최소 8자, 최대 128자, 영문, 숫자, 특수문자 포함 문자열
+  // 이 값은 argon2로 해싱되기 전의 패스워드 값이므로 데이터베이스에 저장 시 반드시 해싱을 해야 함
+  password: string;
+
+  @ApiProperty({
+    nullable: false,
+    description: '닉네임(최소 2자, 최대 50자 문자열)',
+    example: '테스터',
+    type: String,
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 50)
+  // 닉네임 제약조건: 최소 2자, 최대 50자 문자열
+  nickname: string;
+}
+
+// 회원가입 요청 DTO
+export class SignUpRequestDto extends UserDto {}
+
+// 회원가입 응답 DTO
+export class SignupResponseDto extends OmitType(UserDto, ['password']) {}
+
+// 로그인 요청 DTO (필요한 Email, Password만 받음)
+export class SignInRequestDto extends OmitType(UserDto, ['nickname']) {}
+
+// 로그인 응답 DTO
+export class SigninResponseDto extends SignupResponseDto {}
