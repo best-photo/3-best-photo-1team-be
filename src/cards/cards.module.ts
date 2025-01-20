4e668aebd464d1173b6ac31aae2a046918e7273c
heonq <gkagjsrb@naver.com>
2025-01-20 18:23:14 +0900

    fix: 모듈 import,export 추가

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CardsController } from './cards.controller';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [CardsController],
   providers: [CardsService, AuthGuard],
 })
